25a68d77f10662ba4db00314ad91507a5731fcb5
rei <you@example.com>
2022-05-31 21:41:23 +0700

    two for one and mobile fat

--- src/components/UI/icon/InfoIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './InfoIcon.module.css'
-const InfoIcon = ({src}) => {
-    return (
-        <img src={src} className={classes.infoIcon} alt="icon"/>
-    );
-};
-
-export default InfoIcon;
--- src/components/UI/icon/InfoIconMini.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './InfoIconMini.module.css'
-const InfoIconMini = ({src}) => {
-    return (
-        <img src={src} className={classes.infoIconMini} alt="icon"/>
-    );
-};
-
-export default InfoIconMini;

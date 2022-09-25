89a7e63d2ccec58a1546a4c7f385c7da0306ea76
Rei <you@example.com>
2022-09-25 13:10:21 +0700

    ghj

--- src/PC/API/PostService.js
+++ src/PC/API/PostService.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import ResponceHandler from './ResponceHandler'
 export default class PostService {
     static async getSections(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'разделы'
             })
         )
         return responce.data
     }
     static async getSectionMarkers(section){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'метки по разделу',
                 section: section
             })
@@ -20,7 +20,7 @@ export default class PostService {
         return result
     }
     static async getMarker(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'метка',
                 id: id
             })
@@ -31,7 +31,7 @@ export default class PostService {
         return result
     }
     static async getMarkerPhoto(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'фотографии метки',
                 id: id
             })
@@ -40,21 +40,21 @@ export default class PostService {
         return result
     }
     static async getMarkersName(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'имена меток',
             })
         )
         return responce.data
     }
     static async getMarkersCoordinats(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'координаты всех мест',
             })
         )
         return responce.data
     }
     static async getMarkerPrewiev(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'превью метки',
                 id: id
             })
--- src/mobile/API/PostService.js
+++ src/mobile/API/PostService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import ResponceHandler from './ResponceHandler'
 export default class PostService {
     static async getSections(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'разделы'
             })
         )
@@ -21,7 +21,7 @@ export default class PostService {
         return result
     }
     static async getSectionMarkers(section){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'метки по разделу',
                 section: section
             })
@@ -32,7 +32,7 @@ export default class PostService {
         return result
     }
     static async getMarker(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'метка',
                 id: id
             })
@@ -43,7 +43,7 @@ export default class PostService {
         return result
     }
     static async getMarkerPhoto(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'фотографии метки',
                 id: id
             })
@@ -52,21 +52,21 @@ export default class PostService {
         return result
     }
     static async getMarkersName(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'имена меток',
             })
         )
         return responce.data
     }
     static async getMarkersCoordinats(){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'координаты всех мест',
             })
         )
         return responce.data
     }
     static async getMarkerPrewiev(id){
-        const responce = await axios.post(`https://reinaght.xyz`, new URLSearchParams({
+        const responce = await axios.post(`https://route70.ru`, new URLSearchParams({
                 action:'превью метки',
                 id: id
             })
